aecfa07e1117e913fdad55b2b7b48a030df546a6
Jesse Hernandez <jgoboyso@gmail.com>
2026-09-14 15:22:58 -0700

    Fix /supply/catalog landing on the 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -152,6 +152,7 @@ function App() {
           <Route path="/platform" element={<MarketingLayout><Platform /></MarketingLayout>} />
           <Route path="/solutions" element={<MarketingLayout><Solutions /></MarketingLayout>} />
           <Route path="/supply" element={<MarketingLayout><Supply /></MarketingLayout>} />
+          <Route path="/supply/catalog" element={<Navigate to="/supply" replace />} />
           <Route path="/projects" element={<MarketingLayout><Projects /></MarketingLayout>} />
           <Route path="/project" element={<Navigate to="/projects" replace />} />
           <Route path="/projects/:slug" element={<MarketingLayout><ProjectDetail /></MarketingLayout>} />
